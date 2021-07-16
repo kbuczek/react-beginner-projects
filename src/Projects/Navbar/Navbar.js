c4fab46c71698892a8f3e3c802d3a2f27ebfd254
kbuczek <krzysztofbuczek@live.com>
2021-07-16 02:55:38 +0200

    navbar useRef

--- src/Projects/Navbar/Navbar.js
+++ src/Projects/Navbar/Navbar.js
@@ -1,35 +1,66 @@
-import React from "react";
-import { FaBars } from "react-icons/fa";
+import React, { useEffect, useState, useRef } from "react";
+import { FaBars, FaTwitter } from "react-icons/fa";
 import logo from "./logo.png";
+import { links, social } from "./data";
 import "./navbar.css";
 
 const Navbar = () => {
+  const [showLinks, setShowLinks] = useState(false);
+  //useRef doesn't trigger re-render (no useEffect)
+  //used mainly to target DOM elements
+  // refValue.current = <div>...</div>
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
+
+  useEffect(() => {
+    const linksHeight = linksRef.current.getBoundingClientRect();
+    // console.log(linksHeight);
+    if (showLinks) {
+      linksContainerRef.current.style.height = `${linksHeight}px`; //inline css on DOM element linksContainerRef, chagnging height: ""
+    } else {
+      linksContainerRef.current.style.height = "0px";
+    }
+  }, [showLinks]);
+
   return (
     <nav className="nav-nav">
       <div className="nav-center">
         <div className="nav-header">
           <img className="logo" src={logo} alt="company logo" />
-          <button className="nav-toggle">
+          <button
+            className="nav-toggle"
+            onClick={() => setShowLinks(!showLinks)}
+          >
             <FaBars />
           </button>
         </div>
-        <div className="links-container show-container">
-          <ul className="links">
-            <li>
-              <a href="#">home</a>
-            </li>
-            <li>
-              <a href="#">products</a>
-            </li>
-            <li>
-              <a href="#">about us</a>
-            </li>
-            <li>
-              <a href="#">contact</a>
-            </li>
+        <div
+          // className={`${
+          //   showLinks ? "links-container show-container" : "links-container"
+          // }`}
+          className="links-container"
+          ref={linksContainerRef}
+        >
+          <ul className="links" ref={linksRef}>
+            {links.map(({ id, url, text }) => {
+              //all links from map in this return() will be placed in linksRef.current
+              return (
+                <li key={id}>
+                  <a href={url}>{text}</a>
+                </li>
+              );
+            })}
           </ul>
         </div>
-        <ul className="social-icons"></ul>
+        <ul className="social-icons">
+          {social.map(({ id, url, icon }) => {
+            return (
+              <li key={id}>
+                <a href={url}>{icon}</a>
+              </li>
+            );
+          })}
+        </ul>
       </div>
     </nav>
   );
--- src/Projects/Navbar/data.js
+++ src/Projects/Navbar/data.js
@@ -1,52 +1,42 @@
-import React from 'react';
-import { FaBehance, FaFacebook, FaLinkedin, FaTwitter } from 'react-icons/fa';
+import React from "react";
+import { FaBehance, FaFacebook, FaLinkedin, FaTwitter } from "react-icons/fa";
 export const links = [
   {
     id: 1,
-    url: '/',
-    text: 'home',
+    url: "/",
+    text: "home",
   },
   {
     id: 2,
-    url: '/about',
-    text: 'about',
+    url: "/products",
+    text: "products",
   },
   {
     id: 3,
-    url: '/projects',
-    text: 'projects',
+    url: "/about-us",
+    text: "about us",
   },
   {
     id: 4,
-    url: '/contact',
-    text: 'contact',
-  },
-  {
-    id: 5,
-    url: '/profile',
-    text: 'profile',
+    url: "/contact",
+    text: "contact",
   },
 ];
 
 export const social = [
   {
     id: 1,
-    url: 'https://www.twitter.com',
+    url: "https://www.facebook.com",
     icon: <FaFacebook />,
   },
   {
     id: 2,
-    url: 'https://www.twitter.com',
+    url: "https://www.twitter.com",
     icon: <FaTwitter />,
   },
   {
     id: 3,
-    url: 'https://www.twitter.com',
+    url: "https://www.linkedin.com",
     icon: <FaLinkedin />,
-  },
-  {
-    id: 4,
-    url: 'https://www.twitter.com',
-    icon: <FaBehance />,
   },
 ];
