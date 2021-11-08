6cbfbca9d9d8c991513208bfa5df1db808e3426f
kbuczek <krzysztofbuczek@live.com>
2021-11-08 01:23:32 +0100

    typeracer changes

--- src/Projects/TypeRacer/TypeRacer.js
+++ src/Projects/TypeRacer/TypeRacer.js
@@ -5,21 +5,61 @@ import "./TypeRacer.css";
 
 const TypeRacer = () => {
   const [currentWord, setCurrentWord] = useState("");
+  const [currentWordIndex, setCurrentWordIndex] = useState(0);
   const [wordsArray, setWordsArray] = useState([]);
+  const [wordsArrayValues, setWordsArrayValues] = useState([]);
+  // let wordsArrayValues2 = [];
 
   useEffect(() => {
     loadWordsArray();
   }, []);
 
+  useEffect(() => {
+    checkCurrentWord();
+  });
+
   const loadWordsArray = () => {
     setWordsArray(level.text.split(" "));
+    setWordsArrayValues((oldArray) => [...oldArray, "current"]);
+    // wordsArrayValues2.push("current");
+  };
+
+  const checkCurrentWord = () => {
+    if (currentWord === wordsArray[currentWordIndex]) {
+      setWordsArrayValues((wordsArrayValues[currentWordIndex] = "success"));
+      setWordsArrayValues((oldArray) => [...oldArray, "current"]);
+    }
   };
 
   return (
     <main className="typeracer-main container">
-      <div className="typeracer-level">{level.text}</div>
+      <div className="typeracer-level">
+        {wordsArray.map((word, index) => {
+          return (
+            <span
+              // id={`typeracer-word-${index}`}
+              class={`typeracer-word ${
+                wordsArrayValues[index] === "correct"
+                  ? "typeracer-word-correct"
+                  : ""
+              } ${
+                wordsArrayValues[index] === "wrong"
+                  ? "typeracer-word-wrong"
+                  : ""
+              }
+              ${
+                wordsArrayValues[index] === "current"
+                  ? "typeracer-word-current"
+                  : ""
+              }`}
+            >
+              {word}{" "}
+            </span>
+          );
+        })}
+      </div>
       <div>{currentWord}</div>
-      <div>{wordsArray[1]}</div>
+      <div>{wordsArrayValues[0]}</div>
       <div>
         <Keyboard level={level} setCurrentWord={setCurrentWord} />
       </div>
