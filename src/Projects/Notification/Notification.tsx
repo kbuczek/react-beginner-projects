16a090a42b97308331950ff1a72d9a98c0e60698
kbuczek <krzysztofbuczek@live.com>
2022-03-21 17:32:01 +0100

    notification

---
+++ src/Projects/Notification/Notification.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./notification.css";
+
+interface Notification {
+  title: string;
+  message: string;
+}
+
+const Notification: React.FunctionComponent<Notification> = ({
+  title,
+  message,
+}) => {
+  return (
+    <div className="notification">
+      <div className="notification-icon">🐶</div>
+      <div className="notification-text">
+        <div className="notification-text-title">{title}</div>
+        <div className="notification-text-message">{message}</div>
+      </div>
+      <div className="notification-close">X</div>
+    </div>
+  );
+};
+
+export default Notification;
