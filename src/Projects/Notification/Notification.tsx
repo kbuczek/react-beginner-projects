d7edb79e333b73450ba5ef81f551658a9c294fb6
kbuczek <krzysztofbuczek@live.com>
2022-03-22 13:13:02 +0100

    notifications

--- src/Projects/Notification/Notification.tsx
+++ src/Projects/Notification/Notification.tsx
@@ -1,25 +1,38 @@
 import React from "react";
+import {
+  IoInformationCircle,
+  IoCheckmarkCircle,
+  IoAlertCircle,
+  IoCloseCircle,
+  IoClose,
+} from "react-icons/io5";
 import "./notification.css";
 
 interface Notification {
   title: string;
   message: string;
   type: string;
+  dataPosition: string;
 }
 
 const Notification: React.FunctionComponent<Notification> = ({
-  title,
-  message,
-  type = "default",
+  title = "",
+  message = "",
+  type = "info",
+  dataPosition = "top-right",
 }) => {
   return (
-    <div className="notification">
-      <div className="notification-icon">🐶</div>
+    <div className="notification" data-position={dataPosition}>
+      <div className="notification-icon">
+        <IoAlertCircle color="red" size={40} />
+      </div>
       <div className="notification-text">
         <div className="notification-text-title">{title}</div>
         <div className="notification-text-message">{message}</div>
       </div>
-      <div className="notification-close">X</div>
+      <div className="notification-close">
+        <IoClose size={20} />
+      </div>
     </div>
   );
 };
