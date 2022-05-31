6bce9b3c843d33ea4ac772fbf0f5fa0bc5c731d5
kbuczek <krzysztofbuczek@live.com>
2022-05-31 17:35:56 +0200

    color generator

--- src/Projects/ColorGenerator/ColorGenerator.tsx
+++ src/Projects/ColorGenerator/ColorGenerator.tsx
@@ -1,19 +1,42 @@
 import React, { useState } from "react";
 import Values from "values.js";
+import "./ColorGenerator.css";
 
 const ColorGenerator = () => {
   const [color, setColor] = useState("");
   const [error, setError] = useState(false);
-  const [list, setList] = useState([]);
+  const [list, setList] = useState<object[]>([]);
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log("submit");
+    try {
+      let colors = new Values(color).all(10);
+      setList(colors);
+      console.log(list[0]);
+    } catch (error) {
+      console.log(error);
+      setError(true);
+    }
   };
 
   return (
     <>
-      <section className="clrgen-container"></section>
+      <section className="clrgen-container">
+        <h3>color generator</h3>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={color}
+            onChange={(e) => setColor(e.target.value)}
+            // placeholder="#f15025"
+            className={`${error ? "clrgen-error" : null}`}
+          />
+          <button className="clrgen-btn">submit</button>
+        </form>
+      </section>
+      <section className="clrgen-colors">
+        <h4>list goes here</h4>
+      </section>
     </>
   );
 };
--- src/Projects/ColorGenerator/SingleColor.tsx
+++ src/Projects/ColorGenerator/SingleColor.tsx
@@ -1,10 +1,6 @@
 import React, { useState } from "react";
 
 const SingleColor = () => {
-  const [color, setColor] = useState("");
-  const [error, setError] = useState(false);
-  const [list, setList] = useState([]);
-
   return <div>SingleColor</div>;
 };
 
