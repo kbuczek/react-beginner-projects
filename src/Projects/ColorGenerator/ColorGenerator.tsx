764de03cf012e1099abe80d7a34b4a3a290bc18c
kbuczek <krzysztofbuczek@live.com>
2022-06-07 01:22:10 +0200

    singlecolor

--- src/Projects/ColorGenerator/ColorGenerator.tsx
+++ src/Projects/ColorGenerator/ColorGenerator.tsx
@@ -1,18 +1,26 @@
 import React, { useState } from "react";
 import Values from "values.js";
 import "./ColorGenerator.css";
+import SingleColor from "./SingleColor";
 
 const ColorGenerator = () => {
   const [color, setColor] = useState("");
   const [error, setError] = useState(false);
-  const [list, setList] = useState<object[]>([]);
+  const [list, setList] = useState<
+    {
+      rgb: [number, number, number];
+      alpha: number;
+      type: string;
+      weight: number;
+    }[]
+  >([]);
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     try {
       let colors = new Values(color).all(10);
       setList(colors);
-      console.log(list[0]);
+      console.log(list);
     } catch (error) {
       console.log(error);
       setError(true);
@@ -28,14 +36,16 @@ const ColorGenerator = () => {
             type="text"
             value={color}
             onChange={(e) => setColor(e.target.value)}
-            // placeholder="#f15025"
+            placeholder="#f15025"
             className={`${error ? "clrgen-error" : null}`}
           />
           <button className="clrgen-btn">submit</button>
         </form>
       </section>
       <section className="clrgen-colors">
-        <h4>list goes here</h4>
+        {list.map((color, index) => {
+          return <SingleColor key={index} index={index} color={color} />;
+        })}
       </section>
     </>
   );
--- src/Projects/ColorGenerator/SingleColor.tsx
+++ src/Projects/ColorGenerator/SingleColor.tsx
@@ -1,7 +1,33 @@
 import React, { useState } from "react";
+import rgbToHex from "./utils";
 
-const SingleColor = () => {
-  return <div>SingleColor</div>;
+interface SingleColor {
+  index: number;
+  color: {
+    rgb: [number, number, number];
+    alpha: number;
+    type: string;
+    weight: number;
+  };
+}
+
+const SingleColor: React.FunctionComponent<SingleColor> = ({
+  index,
+  color,
+}) => {
+  const [alert, setAlert] = useState(false);
+  const bcg = color.rgb.join(",");
+  const hex = rgbToHex(...color.rgb);
+
+  return (
+    <article
+      className={`singleColor ${index > 10 && "singleColor-light"}`}
+      style={{ backgroundColor: `rgb(${bcg})` }}
+    >
+      <p className="singleColor-weight">{color.weight}%</p>
+      <p className="singleColor-value">{hex}</p>
+    </article>
+  );
 };
 
 export default SingleColor;
