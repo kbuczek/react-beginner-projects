4c4da7bb5fd45efd861363917730915331dd7623
kbuczek <krzysztofbuczek@live.com>
2022-06-24 01:43:37 +0200

    stripe

--- src/Projects/Stripe/context.tsx
+++ src/Projects/Stripe/context.tsx
@@ -1,2 +1,40 @@
 import React, { useState, useContext } from "react";
 import sublinks from "./data";
+interface AppContextInter {
+  isSidebarOpen: boolean;
+}
+
+const AppContext = React.createContext<AppContextInter>({} as AppContextInter);
+
+const AppProvider: React.FC<{ children?: React.ReactNode }> = ({
+  children,
+}) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openSidebar = () => {
+    setIsSidebarOpen(true);
+  };
+
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <AppContext.Provider
+      value={{
+        isSidebarOpen,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
