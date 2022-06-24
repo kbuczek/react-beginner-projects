766a36752d79d31364566ff60717b28553027d82
kbuczek <krzysztofbuczek@live.com>
2022-06-24 16:20:35 +0200

    stripe changes

--- src/Projects/Stripe/HeroStripe.tsx
+++ src/Projects/Stripe/HeroStripe.tsx
@@ -1,8 +1,26 @@
 import React from "react";
+import phoneImg from "./Images/phone.svg";
 import { useGlobalContext } from "./context";
 
 const HeroStripe = () => {
-  return <div>HeroStripe</div>;
+  const { closeSubmenu } = useGlobalContext();
+  return (
+    <section className="heroStripe">
+      <div className="heroStripe-center">
+        <article>
+          <h1>Payments infrastucture for the interet</h1>
+          <p>
+            Millions of companiesof all sizes-from startups to Fortune 500s -
+            use Stripe's osftware and APIs to accept payments, send payouts, and
+            manage their businesses online.
+          </p>
+        </article>
+        <article className="heroStripe-images">
+          <img src={phoneImg} className="heroStripe-phone-img" alt="phone" />
+        </article>
+      </div>
+    </section>
+  );
 };
 
 export default HeroStripe;
--- src/Projects/Stripe/NavbarStripe.tsx
+++ src/Projects/Stripe/NavbarStripe.tsx
@@ -1,7 +1,43 @@
 import React from "react";
+import { FaBars } from "react-icons/fa";
+import { useGlobalContext } from "./context";
+import logo from "./Images/logo.svg";
+import "./NavbarStripe.css";
 
 const NavbarStripe = () => {
-  return <div>NavbarStripe</div>;
+  const { openSidebar, openSubmenu, closeSubmenu } = useGlobalContext();
+
+  return (
+    <nav className="navbarStripe">
+      <div className="navbarStripe-center">
+        <div className="navbarStripe-header">
+          <img src={logo} className="navbarStripe-logo" alt="stripe logo" />
+          <button
+            className="navbarStripe-button navbarStripe-button-toggle"
+            onClick={() => {
+              openSidebar();
+            }}
+          >
+            <FaBars />
+          </button>
+        </div>
+        <ul className="navbarStripe-links">
+          <li>
+            <button className="navbarStripe-link-btn">products</button>
+          </li>
+          <li>
+            <button className="navbarStripe-link-btn">developers</button>
+          </li>
+          <li>
+            <button className="navbarStripe-link-btn">company</button>
+          </li>
+        </ul>
+        <button className="navbarStripe-button navbarStripe-signin">
+          Sign In
+        </button>
+      </div>
+    </nav>
+  );
 };
 
 export default NavbarStripe;
