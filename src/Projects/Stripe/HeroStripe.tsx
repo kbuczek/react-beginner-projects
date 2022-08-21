bc3af40bb4700a69271a2e40f7618b0b3663ecd1
kbuczek <krzysztofbuczek@live.com>
2022-08-21 14:43:04 +0200

    stripe

--- src/Projects/Stripe/HeroStripe.tsx
+++ src/Projects/Stripe/HeroStripe.tsx
@@ -1,19 +1,21 @@
 import React from "react";
 import phoneImg from "./Images/phone.svg";
 import { useGlobalContext } from "./context";
+import "./HeroStripe.css";
 
 const HeroStripe = () => {
   const { closeSubmenu } = useGlobalContext();
   return (
     <section className="heroStripe">
       <div className="heroStripe-center">
-        <article>
-          <h1>Payments infrastucture for the interet</h1>
+        <article className="heroStripe-text">
+          <h1>Payments infrastucture for the internet</h1>
           <p>
-            Millions of companiesof all sizes-from startups to Fortune 500s -
+            Millions of companies of all sizes-from startups to Fortune 500s -
             use Stripe's osftware and APIs to accept payments, send payouts, and
             manage their businesses online.
           </p>
+          <button className="heroStripe-button">Start now</button>
         </article>
         <article className="heroStripe-images">
           <img src={phoneImg} className="heroStripe-phone-img" alt="phone" />
--- src/Projects/Stripe/SidebarStripe.tsx
+++ src/Projects/Stripe/SidebarStripe.tsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { FaTimes } from "react-icons/fa";
+import sublinks from "./data";
+import { useGlobalContext } from "./context";
 
 const SidebarStripe = () => {
-  return <div>SidebarStripe</div>;
+  const { isSidebarOpen, closeSidebar } = useGlobalContext();
+  return (
+    <aside
+      className={`sidebarStripe-wrapper ${
+        isSidebarOpen && "sidebarStripe-show"
+      }`}
+    >
+      <div className="sidebarStripe">
+        <button
+          className="sidebarStripe-close-btn"
+          onClick={() => {
+            closeSidebar();
+          }}
+        >
+          <FaTimes />
+        </button>
+      </div>
+    </aside>
+  );
 };
 
 export default SidebarStripe;
