a2db65ebeed5db940de27a7f9ab92e7067dc3595
kbuczek <krzysztofbuczek@live.com>
2022-09-05 11:42:02 +0200

    stripe changes

--- src/Projects/Stripe/SidebarStripe.tsx
+++ src/Projects/Stripe/SidebarStripe.tsx
@@ -20,6 +20,27 @@ const SidebarStripe = () => {
         >
           <FaTimes />
         </button>
+        <div className="sidebarStripe-links">
+          {sublinks.map((item, index) => {
+            const { page, links } = item;
+            return (
+              <article key={index}>
+                <h4>{page}</h4>
+                <div className="sidebarStripe-sublinks">
+                  {links.map((link, index) => {
+                    const { url, icon, label } = link;
+                    return (
+                      <a key={index} href={url}>
+                        {icon}
+                        {label}
+                      </a>
+                    );
+                  })}
+                </div>
+              </article>
+            );
+          })}
+        </div>
       </div>
     </aside>
   );
