aedc61921d20968db820d72f08810d6d8e46ed09
kbuczek <krzysztofbuczek@live.com>
2022-11-12 18:56:06 +0100

    cartRedux navbar

--- src/Projects/CartRedux/components/Navbar.tsx
+++ src/Projects/CartRedux/components/Navbar.tsx
@@ -2,10 +2,21 @@ import { CartIcon } from "../icons";
 import { useSelector } from "react-redux";
 
 const Navbar = () => {
+  //useSelector((store)=>{}) - get whole store object
+  //most often people use 'state' name instead of 'store' name
+  const amount = useSelector((store: ) => {
+    store.cart.amount;
+  });
   return (
     <nav>
       <div className="nav-center">
         <h3>redux toolkit</h3>
+        <div className="nav-container">
+          <CartIcon />
+          <div className="amount-container">
+            <p className="total-amount">{amount}</p>
+          </div>
+        </div>
       </div>
     </nav>
   );
