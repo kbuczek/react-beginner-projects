9a1d6dff36556874fbf75ef83dba53c2358a63bb
kbuczek <krzysztofbuczek@live.com>
2022-12-04 01:43:57 +0100

    cartSlice data

--- src/Projects/CartRedux/features/cart/cartSlice.ts
+++ src/Projects/CartRedux/features/cart/cartSlice.ts
@@ -1,17 +1,26 @@
 import { createSlice } from "@reduxjs/toolkit";
+import data from "../../data";
 
 //this state will be controlled by store.ts
 
+export interface CartItem {
+  id: string;
+  title: string;
+  price: string;
+  img: string;
+  amount: number;
+}
+
 export interface CartState {
-  cartItems: string[];
+  cartItems: CartItem[];
   amount: number;
   total: number;
   isLoading: boolean;
 }
 
 const initialState: CartState = {
-  cartItems: [],
-  amount: 7,
+  cartItems: data,
+  amount: 0,
   total: 0,
   isLoading: true,
 };
